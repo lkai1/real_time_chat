53036a0dc004434a2c502ce37f21b71597851061
Kai Lehtinen <kai-le@outlook.com>
2023-05-25 15:27:05 +0300

    create userInfoContext

---
+++ frontend/src/Contexts/UserInfoContext.js
@@ -0,0 +1,35 @@
+import { createContext, useEffect, useState, useMemo } from "react";
+import { getUserInfoService } from "../services/userServices";
+
+export const UserInfoContext = createContext({})
+
+const UserInfoProvider = ({ children }) => {
+
+    const [userInfoState, setUserInfoState] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getUserInfoService()
+            .then((userInfo) => {
+                setUserInfoState(userInfo)
+                setLoading(false)
+            })
+            .catch((_error) => { return })
+    }, [])
+
+    const valuesToProvide = useMemo(
+        () => ({
+            userInfoState,
+            loading
+        }),
+        [userInfoState, loading],
+    );
+
+    return (
+        <UserInfoContext.Provider value={valuesToProvide}>
+            {children}
+        </UserInfoContext.Provider>
+    )
+}
+
+export default UserInfoProvider
