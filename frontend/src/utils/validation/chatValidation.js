f9eefd101c0e3a846bcf7dbd7dd8bb9a8091dce7
Kai Lehtinen <kai-le@outlook.com>
2023-05-26 08:42:13 +0300

    create validation for chatId

--- frontend/src/utils/validation/chatValidation.js
+++ frontend/src/utils/validation/chatValidation.js
@@ -14,3 +14,11 @@ export const validateChatName = (chatName) => {
         && chatName.length > 2 && chatName.length < 31
     ) ? true : false
 }
+
+export const validateChatId = (chatId) => {
+    return (
+        chatId
+        && typeof chatId === "string"
+        && chatId.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    ) ? true : false
+}
